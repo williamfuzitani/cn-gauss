fe45a5c7e7ce2c6635f4a57c7024b443302dee68
William Fuzitani <william.fuzitani9594@sistemafiep.onmicrosoft.com>
2021-09-04 12:57:26 -0300

    apresenta resultados e etapas

--- src/scripts/gauss.js
+++ src/scripts/gauss.js
@@ -1,3 +1,5 @@
+// import cloneDeep from "lodash.clonedeep";
+
 let matrizA = [
   [2, 3, -1, 4],
   [-1, 2, 7, 6],
@@ -8,25 +10,35 @@ let matrizA = [
 let b = [5, 3, 9, -1];
 
 function resolveGauss(A, b) {
+  // A = cloneDeep(A);
+  // b = cloneDeep(b);
+  let etapas = [];
   let tamanho = Number(A.length);
 
   for (let linha_pivo = 0; linha_pivo < tamanho - 1; linha_pivo++) {
-    console.log(`Pivo: Matriz[${linha_pivo}][${linha_pivo}]`);
+    let pivo_atual = A[linha_pivo][linha_pivo];
 
     for (let linha = linha_pivo + 1; linha < tamanho; linha++) {
       let multiplicador = A[linha][linha_pivo] / A[linha_pivo][linha_pivo];
+
+      let acao = `L${linha + 1} = L${linha_pivo + 1} - M(${A[linha][linha_pivo]} / ${A[linha_pivo][linha_pivo]}) * L${linha + 1}`;
 
       for (let col = linha_pivo; col < tamanho; col++) {
         A[linha][col] = A[linha][col] - multiplicador * A[linha_pivo][col];
       }
 
       b[linha] = b[linha] - multiplicador * b[linha_pivo];
+
+      console.log({ pivo: pivo_atual, acao: acao, matriz: A, coeficientes: b });
+      etapas.push({ pivo: pivo_atual, acao: acao, matriz: A, coeficientes: b });
+      console.table(etapas);
     }
   }
 
   return {
     matriz: A,
     coeficientes: b,
+    etapas: etapas,
   };
 }
 
@@ -41,14 +53,17 @@ function resolveSistema(A, b) {
       resultado[i] = resultado[i] - resultado[j] * A[i][j];
     }
 
-    resultado[i] = resultado[i] / matriz[i][i];
+    resultado[i] = resultado[i] / A[i][i];
   }
 
   return resultado;
 }
 
-const { matriz, coeficientes } = resolveGauss(matrizA, b);
+const { matriz, coeficientes, etapas } = resolveGauss(matrizA, b);
 
-console.table(matriz);
-console.table(coeficientes);
-console.table(resolveSistema(matriz, coeficientes));
+// console.log(etapas[0]["matriz"]);
+// console.log(etapas[1]["matriz"]);
+// console.log(etapas[2]["matriz"]);
+// console.table(matriz);
+// console.table(coeficientes);
+// console.table(resolveSistema(matriz, coeficientes));
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -17,26 +17,59 @@
         </div>
       </v-col>
     </v-row>
+
+    <h1 class="mt-8">Sistema</h1>
     <v-row>
       <v-col>
+        <v-row v-for="(row, irow) in valores" :key="irow">
+          <v-col v-for="(col, icol) in valores[irow]" :key="icol">
+            <v-text-field
+              v-model.number="row[icol]"
+              type="number"
+              outlined
+              hide-details
+            />
+          </v-col>
+          <v-divider class="mx-8" vertical />
+          <v-col>
+            <v-text-field
+              v-model.number="coeficientes[irow]"
+              type="number"
+              outlined
+              hide-details
+            />
+          </v-col>
+        </v-row>
+      </v-col>
+    </v-row>
+
+    <h1 class="mt-8">Resultado - {{ resultadoSistema }}</h1>
+    <v-row v-for="(etapa, index) in etapas" :key="index">
+      <v-col>
         <v-card outlined>
+          <v-card-title>
+            <div class="d-flex flex-column">
+              <span> Pivo: {{ etapa.pivo }} </span>
+              <span> Equação: {{ etapa.acao }} </span>
+            </div>
+          </v-card-title>
           <v-card-text>
-            <v-row v-for="(row, irow) in valores" :key="irow">
-              <v-col v-for="(col, icol) in valores[irow]" :key="icol">
+            <v-row v-for="(row, irow) in etapa.matriz" :key="irow">
+              <v-col v-for="(col, icol) in etapa.matriz[irow]" :key="icol">
                 <v-text-field
                   v-model.number="row[icol]"
-                  type="number"
                   outlined
                   hide-details
+                  disabled
                 />
               </v-col>
               <v-divider class="mx-8" vertical />
               <v-col>
                 <v-text-field
-                  v-model.number="coeficientes[irow]"
-                  type="number"
+                  v-model.number="etapa.coeficientes[irow]"
                   outlined
                   hide-details
+                  disabled
                 />
               </v-col>
             </v-row>
@@ -48,26 +81,69 @@
 </template>
 
 <script>
+import cloneDeep from "lodash.clonedeep";
+
+function normalizaMatriz(lista) {
+  const tamanho = Number(lista.length);
+
+  for (let row = 0; row < tamanho; row++) {
+    for (let col = 0; col < tamanho; col++) {
+      if (lista[row][col] === "") {
+        lista[row][col] = 0;
+      }
+    }
+  }
+
+  return lista;
+}
+
+function normalizaCoeficientes(lista) {
+  const tamanho = Number(lista.length);
+
+  for (let row = 0; row < tamanho; row++) {
+    if (lista[row] === "") {
+      lista[row] = 0;
+    }
+  }
+
+  return lista;
+}
+
 function resolveGauss(A, b) {
+  A = cloneDeep(normalizaMatriz(A));
+  b = cloneDeep(normalizaCoeficientes(b));
+  let etapas = [];
   let tamanho = Number(A.length);
 
   for (let linha_pivo = 0; linha_pivo < tamanho - 1; linha_pivo++) {
-    // console.log(`Pivo: Matriz[${linha_pivo}][${linha_pivo}]`);
+    let pivo_atual = A[linha_pivo][linha_pivo];
 
     for (let linha = linha_pivo + 1; linha < tamanho; linha++) {
       let multiplicador = A[linha][linha_pivo] / A[linha_pivo][linha_pivo];
+
+      let acao = `L${linha + 1} = L${linha_pivo + 1} - M(${
+        A[linha][linha_pivo]
+      } / ${A[linha_pivo][linha_pivo]}) * L${linha + 1}`;
 
       for (let col = linha_pivo; col < tamanho; col++) {
         A[linha][col] = A[linha][col] - multiplicador * A[linha_pivo][col];
       }
 
       b[linha] = b[linha] - multiplicador * b[linha_pivo];
+
+      etapas.push({
+        pivo: pivo_atual,
+        acao: acao,
+        matriz: cloneDeep(A),
+        coeficientes: b,
+      });
     }
   }
 
   return {
     matriz: A,
     coeficientes: b,
+    etapas,
   };
 }
 
@@ -95,8 +171,7 @@ export default {
     ordem: 3,
     valores: [],
     coeficientes: [],
-    resultadoMatriz: [],
-    resultadoCoeficientes: [],
+    etapas: [],
     resultadoSistema: [],
   }),
 
@@ -112,6 +187,10 @@ export default {
 
   methods: {
     gerarMatriz() {
+      this.valores = [];
+      this.coeficientes = [];
+      this.etapas = [];
+      this.resultadoSistema = [];
       let tamanho = Number(this.ordem);
       let valores = [];
       let coeficientes = [];
@@ -131,11 +210,10 @@ export default {
 
     calcular() {
       const resultadoGauss = resolveGauss(this.valores, this.coeficientes);
-      const { matriz, coeficientes } = resultadoGauss;
+      const { matriz, coeficientes, etapas } = resultadoGauss;
       const resultadoSistema = resolveSistema(matriz, coeficientes);
 
-      this.resultadoMatriz = matriz;
-      this.resultadoCoeficientes = coeficientes;
+      this.etapas = etapas;
       this.resultadoSistema = resultadoSistema;
     },
   },
